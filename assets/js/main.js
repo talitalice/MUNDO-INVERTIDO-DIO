d7fe522b5b148ed8c5d955f35b6ef18987d3de15
Talita Alice <101297710+talitalice@users.noreply.github.com>
2022-11-01 20:27:21 -0300

    Portfolio mundo invertido commitado

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+import { subscribeToHellfireClub } from "./firebase/hellfire-club.js"
+
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+// quando clicar no botão, faça algo
+btnSubscribe.addEventListener('click', async () => {
+    const subscription = {
+        name : txtName.value,
+        email : txtEmail.value,
+        level : txtLevel.value, 
+        character : txtCharacter.value
+    }
+
+    // Salvar no banco de dados!
+    const subscriptionId = subscribeToHellfireClub(subscription)
+
+    txtName.value = ''
+    txtEmail.value = ''
+    txtLevel.value = ''
+    txtCharacter.value = ''
+
+    alert(`Inscrito com sucesso`)
+
+    
+})
